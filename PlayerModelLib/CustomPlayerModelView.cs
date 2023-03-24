27dde717962ef66e2c0eb9459f60b7353237865f
Martoph <sager1018@gmail.com>
2023-03-24 01:12:59 -0500

    Adjust statue location and fix coloration bugs

--- PlayerModelLib/CustomPlayerModelView.cs
+++ PlayerModelLib/CustomPlayerModelView.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Kitchen;
 using KitchenData;
 using KitchenLib.Utils;
@@ -17,16 +18,6 @@ namespace KitchenPlayerModelLib
         private PlayerInfo PlayerInfo;
         
         private static readonly int Color0 = Shader.PropertyToID("_Color0");
-
-        private Material DefaultMaterial
-        {
-            get
-            {
-                var material = MaterialUtils.GetExistingMaterial("Player");
-                material.SetColor(Color0, PlayerInfo.Profile.Colour);
-                return material;
-            }
-        }
 
         private CustomPlayerModelViewData Data;
         
@@ -87,15 +78,23 @@ namespace KitchenPlayerModelLib
 
             this.PlayerInfo = Players.Main.Get(this.Data.PlayerID);
             var playerModel = GameData.Main.Get<PlayerModel>(this.Data.PlayerModelID);
+            var defaultMaterial = new Material(MaterialUtils.GetExistingMaterial("Player"));
+            defaultMaterial.SetColor(Color0, PlayerInfo.Profile.Colour);
             if (playerModel != null)
             {
-                var materials = playerModel.Materials == null || playerModel.Materials.Length == 0 ? new[] {DefaultMaterial} : playerModel.Materials;
+                var materials = new[] {defaultMaterial};
+                if (playerModel.Materials != null && playerModel.Materials.Length > 0)
+                {
+                    materials = new Material[playerModel.Materials.Length];
+                    Array.Copy(playerModel.Materials, materials, playerModel.Materials.Length);
+                }
+                
                 for (var i = 0; i < materials.Length; i++)
                 {
                     var material = materials[i];
                     if (material == null)
                     {
-                        materials[i] = DefaultMaterial;
+                        materials[i] = defaultMaterial;
                     }
                 }
                 
--- PlayerModelLib/SpawnStatue.cs
+++ PlayerModelLib/SpawnStatue.cs
@@ -9,7 +9,7 @@ namespace KitchenPlayerModelLib
     {
         protected override void OnUpdate()
         {
-            var entity = Create(ApplianceReferences.FancyStatue, new Vector3(4f, 0f, 7f), new Vector3(0f, 90f, 0f));
+            var entity = Create(ApplianceReferences.FancyStatue, new Vector3(6f, 0f, 7f), new Vector3(0f, 90f, 0f));
             Set(entity, new CPlayerModelStatue());
         }
     }
